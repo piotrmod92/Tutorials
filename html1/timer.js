47633befeb94ac8c27040deffc42f307e87a8d93
piotrmod92 <piotrmod92@gmail.com>
2016-11-02 17:55:46 +0100

    all tutorials since 2.11.2016 uploaded

--- html1/timer.js
+++ html1/timer.js
@@ -1,6 +1,5 @@
 function odliczanie()
 	{
-		/* funkcja zegar */
 		var dzisiaj = new Date();
 		
 		var godzina = dzisiaj.getHours();
@@ -12,6 +11,7 @@ function odliczanie()
 		var sekunda = dzisiaj.getSeconds();
 		if (sekunda<10) sekunda = "0"+sekunda;
 		
-		document.getElementById("zegar").innerHTML = godzina+":"+minuta+":"+sekunda; 
-		setTimeout("odliczanie()",1000);
+		document.getElementById("zegar").innerHTML = godzina+":"+minuta+":"+sekunda;
+		 
+		 setTimeout("odliczanie()",1000);
 	}
